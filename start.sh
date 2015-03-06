a9f11289356150847e54168620395e4cb10c3669
James Harris <james@rancher.com>
2015-03-06 11:38:03 -0700

    Start with -it

--- start.sh
+++ start.sh
@@ -22,7 +22,7 @@ CONF=$(pwd)/nginx.conf
 PROXY=$(pwd)/docker-registry.conf
 CERTS=$(pwd)/certs
 killAndRemove ${REV_PROXY}
-docker run -d -p 80:80 \
+docker run -it -p 80:80 \
  -p 443:443 \
  -v ${CERTS}:/etc/nginx/certs:ro \
  -v ${HOME}/sandbox/docker-registry.htpasswd:/auth/docker-registry.htpasswd \
@@ -30,5 +30,4 @@ docker run -d -p 80:80 \
  -v ${PROXY}:/etc/nginx/docker-registry.conf \
  --link rancher-registry:registry \
  --name=${REV_PROXY} \
- nginx
- 
+ nginx /bin/bash
