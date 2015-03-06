baacf574e035e942a0662ab681e4d62890a12b56
James Harris <james@rancher.com>
2015-03-06 09:59:58 -0700

    Add auth file in conf

--- start.sh
+++ start.sh
@@ -56,5 +56,6 @@ killAndRemove ${REV_PROXY}
 docker run -d -p 80:80 \
  -p 443:443 \
  -v $(pwd)/certs:/etc/nginx/certs:ro \
+ -v ${HOME}/sandbox/docker-registry.htpasswd:/auth/docker-registry.htpasswd
  --name=${REV_PROXY} \
  nginx
