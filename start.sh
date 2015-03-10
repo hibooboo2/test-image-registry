8a9aa22fa6a833cc4409511b87907eba4725d022
James Harris <james@rancher.com>
2015-03-10 13:46:24 -0700

    no /

--- start.sh
+++ start.sh
@@ -28,7 +28,7 @@ echo ${CURRNETDIR}
 killAndRemove ${REV_PROXY}
 docker run -d -p 80:80 \
  -p 443:443 \
- -v ${CURRNETDIR}:/src/ \
+ -v ${CURRNETDIR}:/src \
  -v ${CERTS}:/etc/nginx/certs:ro \
  -v ${PASSWORDS}:/etc/nginx/.htpasswd \
  -v ${CONF}:/etc/nginx/nginx.conf \
