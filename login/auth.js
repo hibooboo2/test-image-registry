6554ece6d0cf2d10cdc1a7e95e724322384d93d1
James Harris <james@rancher.com>
2015-03-10 16:44:42 -0700

    Change auth flow

--- login/auth.js
+++ login/auth.js
@@ -18,19 +18,20 @@ server.listen(process.env.PORT ? process.env.PORT : 8080);
 app.use('/signup', express.static(__dirname + '/'));
 
 app.post('/signup',function(req, res){
-    res.send("User:"+req.query['username']+"\nPassword:"+req.query['password']
-        + "\n"+ JSON.stringify(req.body));
-    signup(req.body)
+    signup(req.body, req, res)
 })
 
-function signup(user){
+function signup(user, req, res){
     if (user.password && user.username){
         console.log(user);
         auth.add(user,function(err){
             if(!err){
+                res.send("User:"+req.query['username']+"\nPassword:"+req.query['password']
+                + "\n"+ JSON.stringify(req.body));
                 console.log("Added user" + user);
             }
             else{
+                res.send("User already exists.");
                 console.log(err)
                 
             }
